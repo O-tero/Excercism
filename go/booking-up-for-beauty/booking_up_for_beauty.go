ef5bd6f400244f58bf48d97002678f1f5075479a
Otero <oteronickbrian@gmail.com>
2024-03-22 15:44:39 +0300

    gross store

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	scheduleLayout string = "1/2/2006 15:04:05"
-	callLayout      string = "January 2, 2006 15:04:05"
+	callLayout     string = "January 2, 2006 15:04:05"
 	noonLayout     string = "Monday, January 2, 2006 15:04:05"
 	descLayout     string = "You have an appointment on Monday, January 2, 2006, at 15:04."
 )
@@ -12,18 +12,18 @@ const (
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
 	t, err := time.Parse(scheduleLayout, date)
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 	return t
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
 	t, err := time.Parse(callLayout, date)
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 	return time.Now().After(t)
 }
 
@@ -45,5 +45,5 @@ func Description(date string) string {
 // AnniversaryDate returns a Time with this year's anniversary.
 func AnniversaryDate() time.Time {
 	currentYear := time.Now().Year()
-    return time.Date(currentYear, time.September, 15, 0, 0, 0, 0, time.UTC)
+	return time.Date(currentYear, time.September, 15, 0, 0, 0, 0, time.UTC)
 }
