31e8a7c820e8f7dea2c9d91ad4a8ab018a91523e
Otero <oteronickbrian@gmail.com>
2024-07-03 01:56:36 +0300

    word-count

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,21 +1,21 @@
 package wordcount
 
 import (
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 )
 
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-    phrase = strings.ToLower(phrase)
+	phrase = strings.ToLower(phrase)
 
-    re := regexp.MustCompile(`\b\w+('\w+)?\b`)
-    words := re.FindAllString(phrase, -1)
-    freq := make(Frequency)
-    for _, word := range words {
-        freq[word]++
-    }
+	re := regexp.MustCompile(`\b\w+('\w+)?\b`)
+	words := re.FindAllString(phrase, -1)
+	freq := make(Frequency)
+	for _, word := range words {
+		freq[word]++
+	}
 
-    return freq
+	return freq
 }
