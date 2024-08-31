81f36633fdaa6c7c50bf8c73bf04fff3194a5ae6
Otero <oteronickbrian@gmail.com>
2024-08-31 15:04:41 +0300

    contigious series

---
+++ go/series/series.go
@@ -0,0 +1,22 @@
+package series
+
+func All(n int, s string) []string {
+    var result []string
+
+    if n > len(s) {
+        return result
+    }
+
+    for i := 0; i <= len(s)-n; i++ {
+        result = append(result, s[i:i+n])
+    }
+
+    return result
+}
+
+func UnsafeFirst(n int, s string) string {
+    if n > len(s) {
+        return ""
+    }
+    return s[:n]
+}
