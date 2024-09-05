9f402c7281e44875e74bc5d17c7d80aa7ade9c08
Otero <oteronickbrian@gmail.com>
2024-09-05 13:50:18 +0300

    acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,26 @@
+package acronym
+
+import (
+	"strings"
+	"unicode"
+)
+
+func Abbreviate(s string) string {
+	// Split the string using spaces or hyphens as delimiters
+	words := strings.FieldsFunc(s, func(r rune) bool {
+		return r == ' ' || r == '-'
+	})
+// Collect the first letters of each word, ignoring punctuation
+	var acronym string
+	for _, word := range words {
+	// Find the first letter in the word that is a letter (ignore punctuation)
+		for _, char := range word {
+			if unicode.IsLetter(char) {
+				acronym += string(unicode.ToUpper(char))
+				break
+			}
+		}
+	}
+
+	return acronym
+}
